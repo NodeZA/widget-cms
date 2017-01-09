c4468a3540476924cce955e7ac76aa7e8b0254ea
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2017-01-09 15:48:51 +0200

    removed logging

--- server.js
+++ server.js
@@ -125,29 +125,7 @@ module.exports = function (App) {
     });
   }
 
-
-  if (config.saveLogs) {
-
-    let FileStreamRotator = require('file-stream-rotator')
-    let logDirectory = path.join(config.rootDir, 'log');
-
-    // ensure log directory exists
-    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-
-    // create a rotating write stream
-    let accessLogStream = FileStreamRotator.getStream({
-      date_format: 'YYYY-MM-DD',
-      filename: path.join(logDirectory, 'access-%DATE%.log'),
-      frequency: 'daily',
-      verbose: false
-    });
-
-    // setup the logger
-    server.use(morgan('combined', {stream: accessLogStream}));
-  }
-  else {
-    server.use(morgan('dev'));
-  }
+  server.use(morgan('combined'));
 
   // error handling
   if (server.get('env') === 'development') {
